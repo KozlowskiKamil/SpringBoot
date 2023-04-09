77f6775a1bcad37fe8310ac50657d6476aaf1932
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-09 13:52:36 +0200

    Spring boot initial

---
+++ Springboot002/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+delete from users;
+insert into users (name, surname, email, city)
+values ('Ola', 'Kowalska', 'ola@ola.wp', 'Warszawa'),
+       ('Adam', 'Wilk', 'adam@adam.wp', 'Wrocław'),
+       ('Ania', 'Kot', 'ania@ola.wp', 'Bydgoszcz');
