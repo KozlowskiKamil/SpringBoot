b3181eaa64c66deecabe23e5f529d9838fdc0a12
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-09 16:21:30 +0200

    odczyt z bazy danych i wyświetlenie w html

--- Springboot002/src/main/java/com/example/Springboot002/HomeController.java
+++ Springboot002/src/main/java/com/example/Springboot002/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.Springboot002;
 
+import com.example.Springboot002.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
     private final String title = "Test page";
+
+
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
 
     @GetMapping("/home")
     public String home(Model model, @RequestParam(value = "name",
